2417f73e7c485504a0144caa42cbd6d276a5fdb1
snowy-summer <chsb1221@naver.com>
2022-11-24 21:07:51 +0900

    second commit

--- class.py
+++ class.py
@@ -1,54 +1,22 @@
-name = "마린"
-hp = 40
-damage = 5
-print("{0} 유닛이 생성되었습니다.".format(name))
-print("체력 {0}, 공격력 {1}\n".format(hp,damage))
-
-tank_name = "탱크"
-tank_hp = 150
-tank_damage = 35
-print("{0} 유닛이 생성되었습니다.".format(tank_name))
-print("체력 {0},  공격력 {1}\n".format(tank_hp,tank_damage))
-
-def attack(name, location, damage):
-    print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format( name,location, damage))
-
-attack(name, "1 시", damage)
-attack(tank_name, "1시", tank_damage)   
-
-# 유닛이 한마리면 상관이 없는데 몇백마리면 힘들기 때문에 Class를 사용한다.
 
 class Unit:
-    def __init__(self, name, hp, damage): #_init_은 기본으로 쓰자
+    def __init__(self, name, hp, speed,damage): #_init_은 기본으로 쓰자
         self.name = name
         self.hp = hp
         self.damage = damage
+        self.speed = speed
         print("{0} 유닛이 생성 되었습니다.".format(self.name))
         print("체력: {0}, 공격력: {1}\n".format(self.hp, self.damage))
 
-marine1 = Unit("마린", 40, 5)
-marine2 = Unit("마린", 40, 5)
-tank = Unit("탱크", 150, 35)
-
-wraith1 = Unit("레이스", 80, 5)
-print("유닛 이름: {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
-
-
-wraith2 = Unit("레이스", 80, 5)
-wraith2.clocking = True  # Unit에클로킹이라는 변수가 없는데 외부에서 추가로 생성 가능해서 붙혀 넣기가 가능하다.
-
-if wraith2.clocking == True:
-    print("{0} 는 현재 클로킹 상태입니다.".format(wraith2.name))
-
-
-
-
-
-
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다.[속도: {2}]".format(self.name,location, self.speed))
+            
+        
 class AttackUnit(Unit): #Unit에서 상속받는다.
-    def __init__(self,name,hp,damage):  # self는 자기자신 의미  self를 쓰면 자기 자신에게 접근해서 값을 가지고 온다
+    def __init__(self,name,hp,speed,damage):  # self는 자기자신 의미  self를 쓰면 자기 자신에게 접근해서 값을 가지고 온다
         # self가 없을 경우는 받은 값을 그대로 가지고 온다.
-        Unit.__init__(self, name, hp,damage)
+        Unit.__init__(self, name, hp, speed,damage)
       #self.name = name  Unit에서 상속 받기 때문에 필요가 없어진다.
       #self.hp = hp
       #self.damage = damage
@@ -64,7 +32,50 @@ class AttackUnit(Unit): #Unit에서 상속받는다.
             print("{0} 유닛이 파괴되었습니다.".format(self.name))
 
 
-firebat1 = AttackUnit("파이어뱃",50,16) # 파이어뱃 생성
-firebat1.attack("5시") # 5시 방향으로 공격
-firebat1.damaged(25) # 25데미지 입음
-firebat1.damaged(25) #25데미지 입고 체력 0 으로 파괴
+
+# 드랍쉽: 수송기 공격 x
+
+class Flyable: 
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도: {2}]".format(name, location, self.flying_speed))
+
+#공중 공격 유닛 클래스
+class FlyableAttackUnit(AttackUnit, Flyable):  # 다중 상속
+    def __init__(self, name, hp, damage,flying_speed):
+        AttackUnit.__init__(self, name, hp, 0, damage) # 공중유닛은 flying_speed가 있기 때문에 speed를 0으로 해준다.
+        Flyable.__init__(self, flying_speed)
+
+    def move(self,location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
+
+#발키리 : 한번에 14발 공중공격
+        
+valkyrie = FlyableAttackUnit("발키리",200,6,5)
+valkyrie.fly(valkyrie.name, "3시")
+
+#연산자 오버라이딩  자신 클래스에서 정의한 메소드를 사용할때
+
+#벌쳐: 지상유닛 빠름
+vulture = AttackUnit("벌쳐",80,10,20)
+
+battlecruiser = FlyableAttackUnit("배틀쿠르저",500, 25,3)
+
+vulture.move("11시")
+# battlecruiser.fly(battlecruiser.name,"9시")
+battlecruiser.move("9시")
+    
+
+#건물
+
+class BuildingUnit(Unit):
+    def __init__(self, name, hp, location):
+        # Unit.__init__(self,name,hp, 0)
+        super().__init__(name,hp,0) # super는 괄호와 쓰고 self를 안써도 된다.
+        self.location = location
+
+
+
