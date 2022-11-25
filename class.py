4a3609a0041e6686ccab5cc5b621ea134e4533d6
snowy-summer <chsb1221@naver.com>
2022-11-25 23:09:34 +0900

    third commit

--- class.py
+++ class.py
@@ -6,7 +6,14 @@ class Unit:
         self.damage = damage
         self.speed = speed
         print("{0} 유닛이 생성 되었습니다.".format(self.name))
-        print("체력: {0}, 공격력: {1}\n".format(self.hp, self.damage))
+        print("체력: {0}, 공격력: {1}, 속도 : {2}\n".format(self.hp, self.damage, self.speed))
+
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0}: 현제 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} 유닛이 파괴되었습니다.".format(self.name))
 
     def move(self, location):
         print("[지상 유닛 이동]")
@@ -24,13 +31,54 @@ class AttackUnit(Unit): #Unit에서 상속받는다.
     def attack(self, location):
         print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]".format(self.name ,location, self.damage))
 
-    def damaged(self, damage):
-        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
-        self.hp -= damage
-        print("{0}: 현제 체력은 {1} 입니다.".format(self.name, self.hp))
-        if self.hp <= 0:
-            print("{0} 유닛이 파괴되었습니다.".format(self.name))
+    
+class marine(AttackUnit):
+    def __init__(self, name, hp, speed, damage):
+        AttackUnit.__init__(self, "마린", 40, 1, 5)
 
+   #스팀팩: 체력 10 소모, 일정 시간  공격 속도 증가
+
+    def stimpack(self):
+     if self.hp > 10:
+          self.hp-=10
+          self.damage =+ 2
+          print("{0} : 스팀팩을 사용합니다. (HP 10 감소)".format(self.name))
+     else:
+         print(" {0}: 체력이 부족하여 스팀팩을 사용하지 않습니다.".format(self.name))
+
+
+#탱크
+class tank(AttackUnit):
+ #시즈모드 : 탱크 고정 , 데미지 증가 이동 불가
+
+ seize_devel = False
+
+
+ def __init__(self, name, hp, speed, damage):
+    AttackUnit.__init__(self, "탱크", 150, 1, 35)
+    self.seize_mode = False
+   
+ def set_seize_mode(self):
+     if tank.seize_devel == False:
+         return
+     
+
+     #시즈모드 아닐 때 -> 시즈모드
+     if self.seize_mode == False:
+         print("{0} : 시즈모드로 전환합니다.".format(self.name))
+         self.damage*=2
+         self.seize_mode = True
+
+     #현재 시즈모드일 때-> 시즈모드 해제
+     else:
+         print("{0} : 시즈모드를 해제합니다.".format(self.name))
+         self.damage/=2
+         self.seize_mode= False
+         
+       
+       
+
+        
 
 
 # 드랍쉽: 수송기 공격 x
@@ -52,30 +100,37 @@ class FlyableAttackUnit(AttackUnit, Flyable):  # 다중 상속
         print("[공중 유닛 이동]")
         self.fly(self.name, location)
 
-#발키리 : 한번에 14발 공중공격
+# #발키리 : 한번에 14발 공중공격
         
-valkyrie = FlyableAttackUnit("발키리",200,6,5)
-valkyrie.fly(valkyrie.name, "3시")
+# valkyrie = FlyableAttackUnit("발키리",200,6,5)
+# valkyrie.fly(valkyrie.name, "3시")
 
-#연산자 오버라이딩  자신 클래스에서 정의한 메소드를 사용할때
+# #연산자 오버라이딩  자신 클래스에서 정의한 메소드를 사용할때
 
-#벌쳐: 지상유닛 빠름
-vulture = AttackUnit("벌쳐",80,10,20)
+# #벌쳐: 지상유닛 빠름
+# vulture = AttackUnit("벌쳐",80,10,20)
 
-battlecruiser = FlyableAttackUnit("배틀쿠르저",500, 25,3)
+# battlecruiser = FlyableAttackUnit("배틀쿠르저",500, 25,3)
 
-vulture.move("11시")
-# battlecruiser.fly(battlecruiser.name,"9시")
-battlecruiser.move("9시")
+# vulture.move("11시")
+# # battlecruiser.fly(battlecruiser.name,"9시")
+# battlecruiser.move("9시")
     
 
-#건물
+class wraith(FlyableAttackUnit):
 
-class BuildingUnit(Unit):
-    def __init__(self, name, hp, location):
-        # Unit.__init__(self,name,hp, 0)
-        super().__init__(name,hp,0) # super는 괄호와 쓰고 self를 안써도 된다.
-        self.location = location
+    clocking_devel = False
+
+    def __init__(self, name, hp, damage, flying_speed):
+        FlyableAttackUnit.__init__(self, "레이스", 80, 20, 5)
+        self.clocked = False
 
 
-
+    def clocking(self):
+        if self.clocked == True:
+            print("{0} : 클로킹 모드를 해제합니다.".format(self.name))
+            self.clocked = False
+        else:
+            print("{0} : 클로킹 모드 설정합니다.".format(self.name))
+            self.clocked = True
+        
