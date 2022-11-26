4b0d0fcc66b75060f5bd521428012f38ed6e01b1
snowy-summer <chsb1221@naver.com>
2022-11-26 17:44:33 +0900

    final commit

--- class.py
+++ class.py
@@ -1,4 +1,4 @@
-
+from random import*
 class Unit:
     def __init__(self, name, hp, speed,damage): #_init_은 기본으로 쓰자
         self.name = name
@@ -134,3 +134,70 @@ class wraith(FlyableAttackUnit):
             print("{0} : 클로킹 모드 설정합니다.".format(self.name))
             self.clocked = True
         
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다.")
+
+def game_over():
+    print("GG")
+
+
+
+
+#게임 진행
+game_start()
+
+# 마린 3기 생성
+m1= marine("마린", 40, 1, 5)
+m2= marine("마린", 40, 1, 5)
+m3= marine("마린", 40, 1, 5)
+
+#탱크 2기 생성
+t1 = tank("탱크", 150, 1, 35)
+t2 = tank("탱크", 150, 1, 35)
+
+#레이스 1기 생성
+w1= wraith("레이스", 80, 20, 5)
+
+
+#유닛 일괄 관리 (생성된 유닛 append)
+atttack_units = []
+atttack_units.append(m1)
+atttack_units.append(m2)
+atttack_units.append(m3)
+atttack_units.append(t1)
+atttack_units.append(t2)
+atttack_units.append(w1)
+
+#전군 이동
+for unit in atttack_units:
+    unit.move("1시")
+
+#탱크 시즈모드 개발
+tank.seize_devel = True
+print("[알림] 탱크 시즈 모드 개발이 완료되었습니다.")
+
+#공격 모드 준비 (탱크: 시즈모드 레이스 : 클로킹 마린: 스팀팩)
+for unit in atttack_units:
+    if isinstance(unit,marine): # isinstance 지금 만들어진 객체가 클래스의 인스턴스인지 확인
+        unit.stimpack()
+    elif isinstance(unit,tank):
+        unit.set_seize_mode()
+    elif isinstance(unit,wraith):
+        unit.clocking()
+
+        
+# 전군 공격
+for unit in atttack_units:
+    unit.attack("1시")
+
+#전군피해 
+for unit in atttack_units:
+    unit.damaged(randint(5,21)) #데미지 5~21 랜덤으로 받음
+
+#게임 종료
+    game_over()
+
+
+
+
+# 코드를 한번에 안쓰고 여러번에 걸쳐서 쓰니 잘 모르겠다. 이거는 추가적인 공부와 여러번이 필요
